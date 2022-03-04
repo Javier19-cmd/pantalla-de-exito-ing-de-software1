8f9f878955d5af4403d9c015c32cdfa06e209c54
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-03-04 11:18:42 -0600

    cambios

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css'; //Se importa el css de la App.
 
 class App extends Component { //Se creó un componente con la etiqueta de clase. Se pueden tener muchas clases.
     render() {
-        return ( //No quitar el div, porque si no, no renderiza correctamente la página. //Estilo de Bootstrap. Para crear un elemento de HTML, se le debe poner className.
+        return ( //No quitar el div, porque si no, no renderiza correctamente la página. //Para crear un elemento de HTML, se le debe poner className.
             <div className="App">
               <h1 className ="Texto">Su pedido se ha realizado con éxito</h1>
             </div>
